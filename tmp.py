015a92e31d43f79b1a50ec62459d669ba93bcd8c
Ben Tucker <benson.tucker@sitecompli.com>
2014-05-04 19:11:51 -0400

    restructuring

--- tmp.py
+++ tmp.py
@@ -1,10 +1,13 @@
+chartload = {}
 for rec in newData:
 	if not rec['classifier'] in chartload.keys():
 		chartload[rec['classifier']] = {}
+		print "adding " + rec['classifier']
 	if rec['variable'] in chartload[rec['classifier']].keys():
+		print "adding to array " + rec['classifier']
 		tmp_arr = chartload[rec['classifier']][rec['variable']]
 		tmp_arr.append(rec['value'])
 	else:
-		chartload[rec['classifier']]
-		[rec['variable']] = [rec['value']]
+		print "starting new array " + str(rec['variable'])
+		chartload[rec['classifier']][rec['variable']] = [rec['value']]
 	
