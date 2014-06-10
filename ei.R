ad237869d138503815153432dbb513c72ac301e7
Tricia Jamison <tricia.jamison@gmail.com>
2014-06-09 20:32:02 -0400

    Cleaned up R code.

--- ei.R
+++ ei.R
@@ -9,9 +9,11 @@ library(RJSONIO)
 # The goal here is to get the data we want to use into a format that makes it
 # easy for D3, Datamaps, and C3 to display it without a lot of extra
 # Javscript.
-# This happens in two steps - first create a time series data set, then a non-
-# time series data set for all countries.  The two sets for each country
-# are joined at the end when we write the JSON file.
+# This happens in three steps - 
+# 1) first create a time series data set for each country, 
+# 2) second create a non-time series data set for all countries.  
+# 3) Then write the JSON file, joining the two sets for each country.
+#
 # The output of this file is a .JSON file containing our data that the web page
 # will then read in.
 # -----------------------------------------------------------------------------
@@ -109,11 +111,11 @@ str(out)
 # merge with this as an outer join only to insure that we have a set with all 
 # ccodes, even if no resource data for them (those will have no for those)
 # fields).  This could have been done first, would be cleaner.
-base <- read.csv("raw/Country_Classification.csv")
+cclass <- read.csv("raw/Country_Classification.csv")
 str(base)
 
-out_var_cols <- merge(out, base[c(1,2)], by=c("ccode"), all.x=T, all.y=T)
-str(out_var_cols)
+out <- merge(out, cclass[c(1,2)], by=c("ccode"), all.x=T, all.y=T)
+str(out)
 
 
 
@@ -127,41 +129,45 @@ str(out_var_cols)
 
 # -----------------------------------------------------------------------------
 # COUNTRYCLASSIFICATION table
-base <- read.csv("raw/Country_Classification.csv")
-str(base)
-base <- base[,c(1:3,6:8,10)]
+cclass <- read.csv("raw/Country_Classification.csv")
+str(cclass)
+base <- cclass[,c(1:3,6:8,10)]
 
 # -----------------------------------------------------------------------------
 # RESREV table - we only need this for the 'rev_source' classifier
 resrev <- read.csv("raw/Resource_Revenue.csv")
 str(resrev)
 
-outdm <- merge(base, resrev[,c(1,6)], by=c("ccode"), all.x=T);
-outdm <- outdm[!duplicated(outdm),]
-table(outdm$ccode)
-outdm[which(outdm$ccode == "AGO"),]
-outdm$class_opec = factor(outdm$class_opec);
-outdm$class_hipc = factor(outdm$class_hipc);
-outdm$class_ldc = factor(outdm$class_ldc);
-outdm$name <- outdm$country
-outdm <- outdm[,-2]
-str(outdm)
+
+# -----------------------------------------------------------------------------
+# Merge the two 
+out.noTimeSeries <- merge(cclass, resrev[,c(1,6)], by=c("ccode"), all.x=T);
+out.noTimeSeries <- out.noTimeSeries[!duplicated(out.noTimeSeries),]
+table(out.noTimeSeries$ccode)
+out.noTimeSeries[which(out.noTimeSeries$ccode == "AGO"),]
+out.noTimeSeries$class_opec = factor(out.noTimeSeries$class_opec);
+out.noTimeSeries$class_hipc = factor(out.noTimeSeries$class_hipc);
+out.noTimeSeries$class_ldc = factor(out.noTimeSeries$class_ldc);
+out.noTimeSeries$name <- out.noTimeSeries$country
+out.noTimeSeries <- out.noTimeSeries[,-2]
+str(out.noTimeSeries)
 
 # -----------------------------------------------------------------------------
 # This makes a flag indicating whether or not we have any resource data at all
 # for a given country.  This uses the ouput from Step 1.
-d3 <- split(out_var_cols, out_var_cols$ccode)
-d4 <- lapply(d3, function(x) all(is.na(melt(x[,c(9:16)])$value)))
-d5 <- as.data.frame(unlist(d4))
-colnames(d5) = c("haveResourceStats")
-d5$haveResourceStats <- ifelse(d5=="TRUE", 0,1) 
-d5$haveResourceStats <- factor(d5$haveResourceStats)
-d5$ccode <- rownames(d5)
-str(d5)
+tmpByCountry <- split(out, out$ccode)
+tmpDataFlag <- lapply(tmpByCountry, function(x) all(is.na(melt(x[,c(9:16)])$value)))
+tmpDataFlag <- as.data.frame(unlist(tmpDataFlag))
+colnames(tmpDataFlag) = c("haveResourceStats")
+tmpDataFlag$haveResourceStats <- ifelse(tmpDataFlag=="TRUE", 0,1) 
+tmpDataFlag$haveResourceStats <- factor(tmpDataFlag$haveResourceStats)
+tmpDataFlag$ccode <- rownames(tmpDataFlag)
+tmpDataFlag$ccode <- factor(tmpDataFlag$ccode)
+str(tmpDataFlag)
 
 # -----------------------------------------------------------------------------
 # Merges our new flag with the non-time-series data set
-outdm <- merge(outdm, d5, by=c("ccode"), all.x=T, all.y=F)
+out.noTimeSeries <- merge(out.noTimeSeries, tmpDataFlag, by=c("ccode"), all.x=T, all.y=F)
 
 
 # -----------------------------------------------------------------------------
@@ -175,7 +181,7 @@ a <- table(prod$ccode, prod$max.prod.source)
 a <- dcast(as.data.frame(a), Var1 ~ Var2)
 a$prim_prod <- names(a[,2:5])[apply(a[2:5],1,which.max)]
 a$ccode <- a$Var1
-outdm <- merge(outdm, a[,c(7,8)], by=c("ccode"), all.x=T, all.y=F)
+out.noTimeSeries <- merge(out.noTimeSeries, a[,c(7,8)], by=c("ccode"), all.x=T, all.y=F)
 
 
 # -----------------------------------------------------------------------------
@@ -188,19 +194,23 @@ a <- table(rent$ccode, rent$max.rent.source)
 a <- dcast(as.data.frame(a), Var1 ~ Var2)
 a$prim_rent <- names(a[,2:5])[apply(a[2:5],1,which.max)]
 a$ccode <- a$Var1
-outdm <- merge(outdm, a[,c(6,7)], by=c("ccode"), all.x=T, all.y=F)
+out.noTimeSeries <- merge(out.noTimeSeries, a[,c(6,7)], by=c("ccode"), all.x=T, all.y=F)
 
 
 # -----------------------------------------------------------------------------
-# Put all of this into a format to send to JSON encoder.
-d2 <- split(out_var_cols, out_var_cols$ccode)
-#j2 <- lapply(d2, function(x) toJSON(list(ccode=x[1,1],stat=(x[,3:16])), .na="null", digits=14))
-#j2 <- lapply(d2, function(x) toJSON(list(country=outdm[which(outdm$ccode == x[1,1]),],stat=(x[,3:16])), .na="null", digits=14))
-j2 <- lapply(d2, function(x) toJSON(list(country=outdm[which(outdm$ccode == x[1,1]),],macroStats=(x[,3:8]), resourceStats=(x[,c(9,11,13,14,15,16)])), .na="null", digits=14))
-j3 <- paste("[", paste(j2, collapse=","), "]")
+# -----------------------------------------------------------------------------
+# Step 3 
+# Send all this to JSON encoder.  
+# First split by country.
+
+tmpByCountry <- split(out, out$ccode)
+#jsonString <- lapply(tmpByCountry, function(x) toJSON(list(ccode=x[1,1],stat=(x[,3:16])), .na="null", digits=14))
+#jsonString <- lapply(tmpByCountry, function(x) toJSON(list(country=out.noTimeSeries[which(out.noTimeSeries$ccode == x[1,1]),],stat=(x[,3:16])), .na="null", digits=14))
+jsonString <- lapply(tmpByCountry, function(x) toJSON(list(country=out.noTimeSeries[which(out.noTimeSeries$ccode == x[1,1]),],macroStats=(x[,3:8]), resourceStats=(x[,c(9,11,13,14,15,16)])), .na="null", digits=14))
+jsonString <- paste("[", paste(jsonString, collapse=","), "]")
 
 
 # -----------------------------------------------------------------------------
 # Write the file.
-writeLines(j3, "eidata.json")
+writeLines(jsonString, "eidata.json")
 
