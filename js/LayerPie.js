46b76e3fe3bd37f0590828483d78d63be5016d35
Tricia Jamison <tricia.jamison@gmail.com>
2014-07-21 12:04:19 -0400

    More pie chart work.

--- js/LayerPie.js
+++ js/LayerPie.js
@@ -1,11 +1,18 @@
 
-LayerPie = function(element,data) {
+LayerPie = function(element,data, width, height, innerRad) {
   this.element = element;
   var data = data;
-  
-  this.width = 270;
-  this.height = 250;
+  // 270, 250
+  var width = width;
+  var height = height;
+  var innerRad = innerRad; //15, 100
   //var cwidth = 8;
+  var label_height = 20;
+  
+  var center_x = width / 2;
+  var center_y = label_height + (height - label_height) / 2;
+  var outer_radius = d3.min(new Array(center_x, center_y - label_height));
+  console.log("outer_radius" + outer_radius);
   
   var equalchunks = false;
 
@@ -19,7 +26,7 @@ LayerPie = function(element,data) {
   */
 
   //var color = d3.scale.category20();
-  var radius = d3.scale.ordinal().domain(data.map(function(el, ind) { return ind; })).rangeRoundBands([15, 100],.08);
+  var radius = d3.scale.ordinal().domain(data.map(function(el, ind) { return ind; })).rangeRoundBands([innerRad, outer_radius],.07);
   //console.log("rad: " + radius(1));
   //console.log("rad: " + radius.rangeBand());
   //console.log(data.map(function(el, ind) { return ind; }));
@@ -32,23 +39,46 @@ LayerPie = function(element,data) {
 
   var arc = d3.svg.arc();
 
+  var cont = d3.select(this.element).append("svg")
+    .attr("width", width)
+    .attr("height", height);
+    
+  var txt = cont      
+        .append("text")
+        .attr("x", 0)
+        .attr("y", label_height)
+        .attr("width", width)
+        .attr("height", label_height)
+        .attr("class", "datatext")
+        .text("mouse over a pie chunk to see data");
+  var svg = cont.append("g")
+//      .attr("width", width)
+//      .attr("height", height - label_height)        
+    //.attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")")
+    .attr("transform", "translate(" + center_x + "," + center_y + ")")
+    .on("click", function(d){
+      obj.changeChunks();  
+      });
+  
+/*
+// originals below
   var svg = d3.select(this.element).append("svg")
     .attr("width", this.width)
     .attr("height", this.height)
     .append("g")
-    .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")")
+    //.attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")")
+    .attr("transform", "translate(" + center_x + "," + center_y + ")")
     .on("click", function(d){
       obj.changeChunks();  
       });
-    
   var txt = svg.append("text")
-        .attr("x", -120)
-        .attr("y", -110)
-        .attr("width", 80)
+        .attr("x", -(center_x))
+        .attr("y", -(80))
+        .attr("width", center_x)
         .attr("height", 50)
         .attr("class", "datatext")
         .text("mouse over a pie chunk to see data");
-        
+*/        
   var gs, path;
   var obj = this;
   
